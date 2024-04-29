43e9958ff5ca12eb9ad94531abe5e43f03ac0ffb
ruben.dopico.dev <ruben.dopico.dev@gmail.com>
2024-04-29 18:59:12 +0200

    FIX: env variables to use on mysql connection

--- helpers/config.php
+++ helpers/config.php
@@ -1,6 +1,5 @@
 <?php
     $env = parse_ini_file('.env');
-    print_r($env);
     $conn = new mysqli($env['HOST'], $env['USERNAME'], $env['PASSWORD'], $env['DB_NAME'], $env['SQL_PORT']);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
